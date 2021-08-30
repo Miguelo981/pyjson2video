6809573cea5f32c91fcb256e1d9bceb00be2db50
Miguelo981 <lorito20@hotmail.es>
2021-08-30 21:02:29 +0200

    - Language selector added - Logs added

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import json
 
+from loguru import logger
+
 from project.audio.audio_controller import get_audio_duration
 from project.audio.models.voice_engine import VoiceEngine, EXPORT_DIR
 from routes import ROOT_DIR
 
+logger.add("logs.log", backtrace=True, diagnose=True)
+
 with open(ROOT_DIR + r"\inputs\example.json", encoding="utf8") as file:
     data = json.load(file)
-    voice = VoiceEngine(rate=data['voice_speed'])
+    voice = VoiceEngine(rate=data['voice_speed'], lang=data['audio_language'])
 
     for i in range(0, len(data['phrases'])):
         file_route = '\\' + data['title']+'_audio_'+str(i) + data['audio_format']
@@ -15,6 +19,7 @@ with open(ROOT_DIR + r"\inputs\example.json", encoding="utf8") as file:
         data['phrases'][i]['audio']['route'] = EXPORT_DIR + file_route
         data['phrases'][i]['audio']['duration'] = get_audio_duration(EXPORT_DIR + file_route)
         data['phrases'][i]['audio']['volume'] = 0
+        logger.info("Audio: " + EXPORT_DIR + file_route + " created!")
 
     with open('example.json', 'w') as outfile:
         json.dump(data, outfile)
